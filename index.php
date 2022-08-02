08c93a8f22f195c16b169467c66687f9f32efc25
Marvin Virapin <marvin.virapin@hotmail.fr>
2022-08-03 00:45:17 +0200

    plusieurs commit en 1 sorry

--- index.php
+++ index.php
@@ -19,13 +19,8 @@ autoLoad("*.php");
 require __DIR__ . '/vendor/autoload.php';
 
 
-// Définir le fuseau horaire dans lequel le serveur se trouve
 date_default_timezone_set('Europe/Paris');
 
-/* Utiliser include ou require
-* include renvoie un avertissement simple en cas d'erreur
-* require renvoie une erreur fatale et arrête l'exécution du script
-*/
 
 if (verifierAdmin()) 
     require_once './includes/headerAdmin.php';
